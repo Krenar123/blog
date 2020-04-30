8fc48cba488ceca7dc9891e0f62901438a567e43
Krenar123 <krenardardhishta205@gmail.com>
2020-04-30 20:58:01 +0200

    Creation of Article show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
   def index
     @title = 'I am going to buy that Tesla car.'
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
