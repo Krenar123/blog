e61f08efb02347f7006188673fb96c2f2a485da7
Krenar123 <krenardardhishta205@gmail.com>
2020-05-09 17:03:35 +0200

    Added action for creatin new article with handling errors and validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    # validation of what columns ( constraint in sql )
+    validates :title, presence: true, length: {minimum: 5, maximum: 40}
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/articles', to: 'articles#index'
-  # get '/articles/index'
-  # post ...
-  # delete...
+  # E njejta kjo ka me kriju te articles ama post nuk i perzin me get
+  # we have the same route but controller(articles) and action is create...
+  post '/articles', to: 'articles#create'
+  get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+  
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
