1480f11ad7001bb701ad384670e56122fe7c7abd
Krenar123 <krenardardhishta205@gmail.com>
2020-05-19 18:05:24 +0200

    Shared partial file created and added to app.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get '/articles', to: 'articles#index'
-  # E njejta kjo ka me kriju te articles ama post nuk i perzin me get
-  # we have the same route but controller(articles) and action is create...
-  post '/articles', to: 'articles#create'
-  get '/articles/new', to: 'articles#new', as: :new_article
-  get '/articles/:id', to: 'articles#show', as: :article
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article 
-  patch '/articles/:id',to: 'articles#update'
-  delete '/articles/:id',to: 'articles#destroy'
+  resources :articles
   
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
