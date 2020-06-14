2631008160400f20e45b99105099f1a8a7be2fa3
Krenar123 <krenardardhishta205@gmail.com>
2020-06-14 22:45:46 +0200

    Index for user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -6,7 +11,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to root_path
+            redirect_to users_path
         else
             render :new
         end
