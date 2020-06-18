5bacbd3bdcd9587d888b6f46af68435fb0407b9e
Krenar123 <krenardardhishta205@gmail.com>
2020-06-18 23:15:55 +0200

    Show action created and index deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    def index
-        @users = User.all
+    def show
+        @user = User.find(params[:id])
     end
 
     def new
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to users_path
+            redirect_to @user
         else
             render :new
         end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :users
+  resources :users, only: [:show, :new, :create]
 end
