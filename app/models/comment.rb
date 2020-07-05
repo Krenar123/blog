ff6ed423e0604b66738108ccdd53bd185d8bfeb0
Krenar123 <krenardardhishta205@gmail.com>
2020-07-05 23:38:46 +0200

    Made some changes on validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  MINIMUM_BODY_LENGTH = 10
+  MAXIMUM_BODY_LENGTH = 150
   belongs_to :article
   belongs_to :user
+  validates :body, presence: true,  length: {minimum: MINIMUM_BODY_LENGTH, maximum: MAXIMUM_BODY_LENGTH}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
   context "when saving" do
     it "transform email to lower case" do
       user = create(:user,email:'TEST@TEST.com')
       expect(user.email).to eq('test@test.com')
     end
   end
-
-  describe "validations" do
-    it { is_expected.to validate_presence_of(:name)}
-    
-    it { is_expected.to validate_presence_of(:email)}
-
-    it { is_expected.to validate_presence_of(:password)}
-        
+  describe "authentications" do
     it { is_expected.to have_secure_password}
+    
+    it { is_expected.to have_many(:comments)}
+
+    it { is_expected.to have_many(:articles)}
+
+    describe "when deleting user" do
+      let(:comment_count) { 1 }
+      let(:article_count) { 1 }
+      let(:user) { create(:user) }
+      it 'should delete all comments' do
+        create_list(:comment, comment_count, user: user)
+        expect { user.destroy }.to change{ Comment.count }.by(-comment_count)
+      end
+
+      it 'should delete all articles' do
+        create_list(:article, article_count, user: user)
+        expect { user.destroy }.to change{ Article.count }.by(-article_count)
+      end
+    end
+  end
+  describe "validations" do
+    context 'when trying to save empty fields' do
+      it { is_expected.to validate_presence_of(:name)}
+      
+      it { is_expected.to validate_presence_of(:email)}
   
+      it { is_expected.to validate_presence_of(:password)}
+    end
+        
     it { is_expected.to validate_length_of(:password).is_at_least(User::MINIMUM_PASSWORD_LENGTH)}
 
     it { is_expected.to validate_length_of(:name).is_at_most(User::MAXIMUM_NAME_LENGTH)}
 
     it { is_expected.to validate_length_of(:email).is_at_most(User::MAXIMUM_EMAIL_LENGTH)}
     
-    it { is_expected.to have_many(:articles) }
-    
-    context 'other validate' do
+    context 'when email is the same' do
       subject { create(:user) }
       it {  is_expected.to validate_uniqueness_of(:email) }
     end
